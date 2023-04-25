647309380acb230a0e369a9dd746548bf70231d1
Anuja Jayathilaka <anuja.jayathilaka@cammsgroup.com>
2023-04-25 12:21:25 +0530

    Add stratup,update nugets and change web API project

---
+++ Application.Core/ServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Core
+{
+    public static class ServiceRegistration
+    {
+        public static void AddApplication(this IServiceCollection services, IConfiguration configuration)
+        {
+            
+        }
+
+   
+    }
+}
---
+++ Infrastructure.Core/ServiceRegistration.cs
@@ -0,0 +1,18 @@
+﻿using Application.Interface;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Core
+{
+    public static class ServiceRegistration
+    {
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnectionString")));
+            services.AddScoped(typeof(IUnitOfWork), typeof(UnitOfWork));
+           
+        }
+    }
+}
